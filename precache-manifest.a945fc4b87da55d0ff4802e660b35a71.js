e6de79b019f078792c2bf2de3927ecd51d5f2df2
Ruslan <ruslan.astratov@yandex.ru>
2021-04-23 16:50:50 +0200

    Updates

---
+++ precache-manifest.a945fc4b87da55d0ff4802e660b35a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54805aecde11ba34d802ef41929bc0ee",
+    "url": "/magazine-on-react-redux/index.html"
+  },
+  {
+    "revision": "b236f2e06f0d693266fb",
+    "url": "/magazine-on-react-redux/static/css/main.4eddbd39.chunk.css"
+  },
+  {
+    "revision": "12cb901f9703cabbfa75",
+    "url": "/magazine-on-react-redux/static/js/2.406da245.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/magazine-on-react-redux/static/js/2.406da245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b236f2e06f0d693266fb",
+    "url": "/magazine-on-react-redux/static/js/main.896c4d18.chunk.js"
+  },
+  {
+    "revision": "1ac0b7c0a1aef9d01074",
+    "url": "/magazine-on-react-redux/static/js/runtime-main.d9869128.js"
+  }
+]);
